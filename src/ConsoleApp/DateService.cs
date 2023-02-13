ca5e308be9d9d336441a3bdd6494aa52d1fd3007
gmswapps <50689734+gmswapps@users.noreply.github.com>
2023-02-13 14:12:45 +0800

    Get week no

---
+++ src/ConsoleApp/DateService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class DateService
+    {
+        public static int GetWeekNo(CultureInfo cultureInfo, DateTime? date = null)
+        {
+            var givenDate = DateTime.Now;
+            if(date.HasValue)
+            {
+                givenDate = date.Value;
+            }
+            //CultureInfo norwayCulture = new CultureInfo("nn-NO"); 
+            Console.WriteLine(date?.ToLongDateString()); 
+            int weekno = cultureInfo.Calendar
+                                        .GetWeekOfYear(givenDate, CalendarWeekRule.FirstFullWeek,
+                                                       DayOfWeek.Sunday); 
+            return weekno;
+        }
+    }
+}
---
+++ src/ConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp;
+using System.Globalization;
+
+Console.WriteLine("Hello, World!");
+
+//Date
+int weekNo = DateService.GetWeekNo(new CultureInfo("nn-NO"));
+Console.WriteLine($"Week no : {weekNo}");
